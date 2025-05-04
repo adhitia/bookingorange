74ea79e6868021a9c289c1299c7855c305dda7af
adhitia <adhitia.hidayat@gmail.com>
2025-05-04 22:57:03 +0700

    3 day in request

--- app/controllers/api/v1/schedules_controller.rb
+++ app/controllers/api/v1/schedules_controller.rb
@@ -20,36 +20,36 @@ module Api
   
         def build_timeslots(branch, start_date, end_date)
           booking_duration = 30.minutes
-
-
-          day_name = convert_to_indonesian(start_date.strftime("%A"))
-          schedules = branch.schedules.where(day: day_name)
           slots = []
-          schedules.each do |schedule|
-            slot_time = schedule.start_time
 
-            while (slot_time + booking_duration) <= schedule.end_time
-              candidate_start = slot_time
-              candidate_end   = slot_time + booking_duration
-              
-              conflict = Booking.where(schedule_id: schedule.id, booking_date: start_date)
-                                .where.not(status: 'canceled')
-                                .where("booking_time < ? AND booking_end_time > ?", candidate_end, candidate_start)
-                                .exists?
+          (start_date..end_date).each do |date|
+            day_name = convert_to_indonesian(date.strftime("%A"))
+            schedules = branch.schedules.where(day: day_name)
+            schedules.each do |schedule|
+              slot_time = schedule.start_time
 
-              unless conflict
-                slots << {
-                  date: start_date,
-                  start_time: slot_time.strftime("%H:%M"),
-                  end_time: (slot_time + booking_duration).strftime("%H:%M"),
-                  available: true
-                }
+              while (slot_time + booking_duration) <= schedule.end_time
+                candidate_start = slot_time
+                candidate_end   = slot_time + booking_duration
+                
+                conflict = Booking.where(schedule_id: schedule.id, booking_date: date)
+                                  .where.not(status: 'canceled')
+                                  .where("booking_time < ? AND booking_end_time > ?", candidate_end, candidate_start)
+                                  .exists?
+
+                unless conflict
+                  slots << {
+                    date: date,
+                    start_time: slot_time.strftime("%H:%M"),
+                    end_time: (slot_time + booking_duration).strftime("%H:%M"),
+                    available: true
+                  }
+                end
+    
+                slot_time += booking_duration
               end
-  
-              slot_time += booking_duration
             end
           end
-
           return slots
         end
 
